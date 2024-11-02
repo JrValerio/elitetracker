efc9238c51157f1a249eab528961e3388c343b3b
JrValerio <amarovsjr81@gmail.com>
2024-11-02 15:08:11 -0300

    chore: configure ESLint and Prettier for code standardization

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: tsParser,
+      globals: { ...globals.node, ...globals.browser },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      ...prettier.rules,
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+];
--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,9 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
-app.listen(4000, () => console.log('🚀 Server running on port 4001'));
+app.listen(4000, () => console.log('🚀 Server running on port 4000'));
 
 export default app;
-
