a7d29d9303f22c38e8f26ec85e60826e0fcb7d67
JrValerio <amarovsjr81@gmail.com>
2024-11-03 10:29:35 -0300

    feat(database): add MongoDB setup with Docker integration
    - Configured MongoDB connection with Mongoose
    - Added Docker support for MongoDB containerization
    - Set up environment variables for MongoDB URI

---
+++ src/database/index.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const mongoURI =
+  process.env.MONGO_URI || 'mongodb://localhost:27017/elitetracker';
+export async function setupMongo() {
+  try {
+    if (mongoose.connection.readyState === 1) {
+      console.log('Already connected to MongoDB.');
+      return;
+    }
+    console.log('Connecting to MongoDB...');
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI is not defined');
+    }
+    await mongoose.connect(mongoURI);
+    console.log('Connected to MongoDB successfully');
+  } catch (error) {
+    console.log('Error connecting to MongoDB', error);
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
+import 'dotenv/config';
 import express from 'express';
 
+import { setupMongo } from './database';
 import { routes } from './routes';
 
 const app = express();
-app.use(express.json());
-app.use(routes);
 
-app.listen(4000, () => console.log('🚀 Server running on port 4000'));
+setupMongo().then(() => {
+  app.use(express.json());
+  app.use(routes);
+
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+});
 
 export default app;
