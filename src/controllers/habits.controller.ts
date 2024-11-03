dd7d7a90386b4fb5eac3b0d97a0faa720d8f0829
JrValerio <amarovsjr81@gmail.com>
2024-11-03 11:33:23 -0300

    feat(schema): add isCompleted field to Habit schema for tracking completion dates
    - Added 'isCompleted' field as an array of dates within the Habit schema
    - Allows tracking multiple completion dates in a single document
    - MongoDB supports up to ~125k entries, depending on document size

--- src/controllers/habits.controller.ts
+++ src/controllers/habits.controller.ts
@@ -1,11 +1,8 @@
 import { Request, Response } from 'express';
 export class HabitsController {
-  private readonly habits: { name: string }[] = [];
   create = (req: Request, res: Response) => {
     const { name } = req.body;
     const newHabit = { name };
-
-    this.habits.push(newHabit);
 
     res.status(201).json(newHabit);
   };
